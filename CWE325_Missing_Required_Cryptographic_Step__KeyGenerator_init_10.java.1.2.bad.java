d089a295795981bd5b48ab4dc83652a7725b36e0
Sergiu Zaharia <sergiuzaharia@yahoo.com>
2022-10-21 00:14:09 +0300

    Add files via upload

---
+++ CWE325_Missing_Required_Cryptographic_Step__KeyGenerator_init_10.java.1.2.bad.java
@@ -0,0 +1,16 @@
+    public void bad() throws Throwable
+    {
+        if (IO.staticTrue)
+        {
+            final String CIPHER_INPUT = "ABCDEFG123456";
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+            SecretKey secretKey = keyGenerator.generateKey();
+            byte[] byteKey = secretKey.getEncoded();
+            SecretKeySpec secretKeySpec = new SecretKeySpec(byteKey, "AES");
+            Cipher aesCipher = Cipher.getInstance("AES");
+            aesCipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+            byte[] encrypted = aesCipher.doFinal(CIPHER_INPUT.getBytes("UTF-8"));
+            IO.writeLine(IO.toHex(encrypted));
+        }
+    }
+
